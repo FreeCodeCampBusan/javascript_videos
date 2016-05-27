74338cfe811df5d278ff231937dff4f0280cd090
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-27 16:32:07 +0900

    Video 0: Function Basics

---
+++ 0_functions.js
@@ -0,0 +1,15 @@
+function sayGreeting(greeting) {
+  console.log(greeting +  " Free Code Camp!");
+}
+
+sayGreeting("Hello");
+sayGreeting("Hola");
+
+function addTwoNumbers(a, b) {
+  console.log("This will run");
+  return a + b;
+  console.log("This will not run!");
+}
+
+var sum = addTwoNumbers(5,3);
+console.log(sum);
