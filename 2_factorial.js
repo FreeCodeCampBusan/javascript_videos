ac7a18a056694eca1259ca3b2cdd30a6bdc7c094
Stephen Mayeux <stephenmayeux@gmail.com>
2016-05-31 17:17:22 +0900

    update function to return 1 for 0 factorial

--- 2_factorial.js
+++ 2_factorial.js
@@ -3,16 +3,20 @@
 // Using iteration and functional programming
 function factorial_i(num) {
 
-  // create a range of numbers 1 through num
-  var array = [];
-  for (var i = 1; i <= 5; i++) {
-    array.push(i);
-  }
+  if (num === 0) {
+    return 1;
+  } else {
+    // create a range of numbers 1 through num
+    var array = [];
+    for (var i = 1; i <= num; i++) {
+      array.push(i);
+    }
 
-  // reduce the array of integers to one value
-  return array.reduce(function(a, b) {
-    return a * b;
-  });
+    // reduce the array of integers to one value
+    return array.reduce(function(a, b) {
+      return a * b;
+    });
+  }
 }
 
 // Using recursion
@@ -20,12 +24,14 @@ function factorial_r(num) {
 
   // This is the base case.
   // Without it, you will have an infinite loop!
-  if (num === 1) {
+  if (num <= 1) {
     return 1;
   } else {
-    return num * factorial(num-1);
+    return num * factorial_r(num-1);
   }
 }
 
 console.log("Using iteration: " + factorial_i(5));
 console.log("Using recursion: " + factorial_r(7));
+console.log("Using iteration: " + factorial_i(0));
+console.log("Using recursion: " + factorial_r(0));
